b6e4df0dd35bd64308221f224ef10871c61c21a2
hashdoge <hashdoggie@gmail.com>
2018-02-12 17:10:17 +0000

    changed postgres settings

--- StackOverflowVisual/settings.py
+++ StackOverflowVisual/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'stack',
-        'USER': 'hash',
-        'PASSWORD': 'c14498008',
+        'USER': 'root',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
