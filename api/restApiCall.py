e14269d3ca54c12cdb25ae5902ad3b09eea4aa57
Hashdoge <hashdoggie@gmail.com>
2018-03-17 13:58:13 +0000

    made changes to restApiCall.py

--- api/restApiCall.py
+++ api/restApiCall.py
@@ -1,21 +1,20 @@
 import requests
-import json
 import psycopg2
 
 
 conn = psycopg2.connect("host = 'localhost' port='5432' dbname='stack' user='root' password='root'")
 
-req = requests.get('https://api.stackexchange.com/2.2/tags?fromdate=1420070400&order='
-                 'desc&min=1&max=1000&sort=popular&site=stackoverflow')
+req = requests.get('https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow')
 
 data = req.json()
 
 cur = conn.cursor()
 
+cur.execute('truncate api_tags')
 for each in data['items']:
     countField = each['count']
     nameField = each['name']
-    cur.execute("INSERT INTO api_tags(count,name) VALUES (%s, %s)",
+    cur.execute('INSERT INTO api_tags(count,name) VALUES (%s, %s)',
                 (countField, nameField))
 
 conn.commit()
